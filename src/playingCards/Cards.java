40f6386c02c20ec9220719cb8a00d97806f95a46
Kevin Wang <wang182@sheridancollege.ca>
2022-02-01 16:06:41 -0500

    parttimestudent class added

--- src/playingCards/Cards.java
+++ src/playingCards/Cards.java
@@ -1,10 +1,8 @@
 package playingCards;
 
-public class Cards {
-    private Card[] Card = new Card[52]; // 52 card deck
+public abstract class Cards {
+    protected Card[] card = new Card[52]; // 52 card deck
     
     // Card array accessor
-    public Card[] getCard() {
-        return this.Card;
-    }
+    abstract Card[] getCard();
 }
