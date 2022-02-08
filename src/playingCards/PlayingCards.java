a6513e24703403a75c5dd25fbc5ea02efe60df96
Kevin Wang <wang182@sheridancollege.ca>
2022-02-08 10:56:58 -0500

    Kevin(Yi Fei) Wang : 991596501
    Created new Card object

--- src/playingCards/PlayingCards.java
+++ src/playingCards/PlayingCards.java
@@ -7,15 +7,16 @@ public class PlayingCards {
 	Deck deck = new Deck();
 	deck.create();
         deck.showCards();
-
-	System.out.println("Sorted cards");
-	deck.sort();
-	deck.showCards();
+        
+//	System.out.println("Sorted cards");
+//	deck.sort();
+//	deck.showCards();
 
 	System.out.println("Shuffled cards");
 	deck.shuffle();
 	deck.showCards();
         
+        // Moved sort to end since deck is created already sorted
         System.out.println("Sorted cards");
 	deck.sort();
 	deck.showCards();
